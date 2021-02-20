7bbffcd46a2adb097ca8d57e9521f2f1e990202f
Henning Bragge <henning.bragge@gmail.com>
2021-02-20 08:29:17 -0800

    Init

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 import pandas as pd
 
 
+df = pd.read_excel('book.xlsx', index_col=None)
+print(df)
+dct = df.to_dict('records')
+print(dct)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    pass
